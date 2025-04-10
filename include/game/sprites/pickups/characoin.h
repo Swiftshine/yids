2582b89019eb35198e85826e23a8a0446037b966
YoshiDonoshi <github@donoshi.com>
2025-04-10 14:51:01 -0700

    First functions located

---
+++ include/game/sprites/pickups/characoin.h
@@ -0,0 +1,6 @@
+struct CharacterCoin;
+
+void CharacterCoin_Create(CharacterCoin* sprite/*,more?*/);
+void CharacterCoin_Update(CharacterCoin* sprite);
+void CharacterCoin_Render(CharacterCoin* sprite);
+void CharacterCoin_Destroy(CharacterCoin* sprite);

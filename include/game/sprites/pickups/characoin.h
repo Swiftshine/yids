570e23f8762cd13a6842cba2b8c924e79973566b
YoshiDonoshi <github@donoshi.com>
2025-04-10 14:53:11 -0700

    Small update

--- include/game/sprites/pickups/characoin.h
+++ include/game/sprites/pickups/characoin.h
@@ -1,6 +1,13 @@
-struct CharacterCoin;
+#ifndef CHARACOIN_H
+#define CHARACOIN_H
+
+struct CharacterCoin {
+    
+};
 
 void CharacterCoin_Create(CharacterCoin* sprite/*,more?*/);
 void CharacterCoin_Update(CharacterCoin* sprite);
 void CharacterCoin_Render(CharacterCoin* sprite);
 void CharacterCoin_Destroy(CharacterCoin* sprite);
+
+#endif
